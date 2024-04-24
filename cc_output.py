9b567fed0a9060a844009733739891f3a2b4c47f
zxctapochek1000-7 <165178897+zxctapochek1000-7@users.noreply.github.com>
2024-04-24 19:37:27 +0400

    Create cc_output.py

---
+++ cc_output.py
@@ -0,0 +1,23 @@
+def show_start_message() -> None:
+    ""''
+    print("Данная программа реализует операции с шифрованием")
+    """"""
+def show_continue_message() -> None:
+    """"""
+    print("Желаете продолжить? (да/нет)")
+    ""''''''
+def show_finish_message() -> None:
+    """"""
+    print("Завершение работы")
+    """"""""
+def show_options_message() -> None:
+    """"""
+    print("Выберите опцию из списка (укажите номер 1-4):")
+    print("1. Шифр Атбаша")
+    print("2. Шифр Цезаря")
+    print("3. Азбука Морзе")
+    print("4. Выход")
+def show_option_error_message() -> None:
+    """"""
+    print("Номер опции введен не верно: выберете число от 1 до 4:e")
+    """"""
